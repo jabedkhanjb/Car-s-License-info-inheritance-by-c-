941e1bcbc32d544d3c76e7652ad9aba2c977a170
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-09-25 13:54:05 +0600

    inheritance and method overriding

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// Create a Inheritance of Car with a class demonstrating 
+// Car information of Father & that same car is inherited by the son so display Car information of Child
+
+// Car class will have members:
+// ```
+// noOfWheels
+// CarLicensPlateRegNo
+// CarUserName
+// CarUserLicenceNo
+
+using System;
+class Car
+{
+    public void noOfWheels( int noOfWheels = 4)
+    {
+        Console.WriteLine("Car's Wheels : " + noOfWheels);
+    }
+    public int CarLicensPlateRegNo = 240523625;
+
+    public virtual void CarUserName (string CarUserName)
+    {
+        Console.WriteLine("User's name : " + CarUserName);
+    }
+    public virtual void CarUserLicenceNo (int CarUserLicenceNo)
+    {
+        Console.WriteLine("User's License number : " + CarUserLicenceNo);
+    }
+
+}
+class Father : Car
+{
+    public override void CarUserName(string CarUserName)
+    {
+        base.CarUserName(CarUserName);
+    }
+    public override void CarUserLicenceNo(int CarUserLicenceNo)
+    {
+        base.CarUserLicenceNo(CarUserLicenceNo);
+    }
+}
+class Son : Car
+{
+    public override void CarUserName(string CarUserName)
+    {
+        base.CarUserName(CarUserName);
+    }
+    public override void CarUserLicenceNo(int CarUserLicenceNo)
+    {
+        base.CarUserLicenceNo(CarUserLicenceNo);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car Car = new Car();
+        Car Father = new Father();
+        Father.CarUserName("Mahbub Islam Khan Parvez");
+        Father.CarUserLicenceNo(110225315);
+        Console.WriteLine("Car's License number : " + Car.CarLicensPlateRegNo);
+        Father.noOfWheels();
+
+        Console.WriteLine("\n");
+        
+        Car Son = new Son();
+        Son.CarUserName("Mahfuz Islam Khan jabed");
+        Son.CarUserLicenceNo(560305922);
+        Console.WriteLine("Car's License number : " + Car.CarLicensPlateRegNo);
+        Son.noOfWheels();
+
+        
+    }
+}
